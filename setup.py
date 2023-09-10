3fbe537bd73b0cfe036f6b6a3558b1b68c82f3e7
SiddheshKanawade <siddhesh.kanawade@iitgn.ac.in>
2023-09-10 23:18:51 +0530

    build skeleton and setup dependencies

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+__version__ = "0.0.1"
+
+setup(
+    name="get-notifications",
+    version=__version__,
+    packages=find_packages(),
+    install_requires=[],
+)
