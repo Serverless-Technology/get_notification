0a493dfdc24392172b7230205e86a29143caae6d
Mann Jain <mann.jain@iitgn.ac.in>
2023-11-05 11:05:06 +0530

    Sync with git

--- get_notification/handler.py
+++ get_notification/handler.py
@@ -4,9 +4,9 @@ import json
 def hello(event, context):
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+        "event": event
     }
-
+    print(event)
     response = {
         "statusCode": 200,
         "body": json.dumps(body)
